bb5a02dfdafa5d6a5c5335a68b3603aca4c75604
Harel <hareldor@gmail.com>
2015-08-22 02:33:59 -0700

    Add comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow users to create a matrix wrapper which caches its
+## inverse after the first calculation, saving time on future calculations.
 
-## Write a short comment describing this function
+## Make variables for the matrix and its inverse and store them in the
+## calling environment of the function, then return a list of functions
+## used to access and modify them. By default, the matrix is empty, though it
+## can be specified when calling the function. Setting the matrix again will
+## clear the cached inverse, forcing it to be recalculated.
 
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
@@ -18,7 +22,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Take matrix wrapper as input, check if inverse has already been calculated,
+## and - if not - calculate it. Returns matrix inverse. Extra arguments are
+## given to solve() function. Assumes matrix is invertible.
+## 
+## Side-effects: prints message if cached inverse is found.
 
 cacheSolve <- function(x, ...) {
         inv <- x$getinv()
